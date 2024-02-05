1bd3a4addd704840de07139e8fe97dd164145bc0
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-05 10:34:21 +0530

    no need for d

--- Logical/Pallindrome.cs
+++ Logical/Pallindrome.cs
@@ -10,9 +10,8 @@
             }
             int n = x;
             int rev = 0;
-            while (n > 0) {
-                int d = n%10;
-                rev = rev * 10 + d;
+            while (n > 0) {      
+                rev = rev * 10 + n % 10;
                 n = n / 10;
             }
             return rev == x;
