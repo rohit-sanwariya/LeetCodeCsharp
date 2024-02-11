dec7dd51150a35142a85b3f2488e18f32f479784
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-11 20:09:30 +0530

    Blind 75 -> day 1 - 121

---
+++ Blind75/Day1 best-time-to-buy-and-sell-stock/SlidingWindow.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+namespace Blind75.Day1;
+
+public class SlidingWindow
+{
+    public int maxProfit(int[] prices)
+    {
+        int max = 0;
+        int buy =  0;
+        int sell = 1;
+       while(sell<prices.Length)
+        {
+            max = prices[sell] - prices[buy] > max ? prices[sell] - prices[buy] : max;
+            if (prices[buy] > prices[sell])
+            {
+               
+                buy = sell;
+
+        
+            }
+           
+               
+                sell++;
+
+         
+        }
+
+        return max;
+    }
+}
---
+++ Blind75/Program.cs
@@ -0,0 +1,13 @@
+﻿using Blind75.Day1;
+
+namespace Blind75
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SlidingWindow slide = new();
+            Console.WriteLine(slide.maxProfit([2, 4, 1]));
+        }
+    }
+}
