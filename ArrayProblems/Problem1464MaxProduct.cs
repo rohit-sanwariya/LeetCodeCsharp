60817bdb7450747d6f8c7835aad9f2ff7b001e8e
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-02-12 18:59:37 +0530

    optimised with smax max.

--- ArrayProblems/Problem1464MaxProduct.cs
+++ ArrayProblems/Problem1464MaxProduct.cs
@@ -1,17 +1,33 @@
 ﻿ 
 
-namespace ArrayProblems
+namespace ArrayProblems;
+
+public class Problem1464MaxProduct
 {
-    public class Problem1464MaxProduct
+    public int MaxProduct(int[] nums)
     {
-        public int MaxProduct(int[] nums)
+        if (nums.Length == 2)
         {
-            if (nums.Length == 2)
-            {
-                return nums[0] - 1 * nums[1] - 1;
-            }
-            Array.Sort(nums, new Comparison<int>((a, b) => b.CompareTo(a)));
             return (nums[0] - 1) * (nums[1] - 1);
+        }
+        else
+        {
+            int max = -1;
+            int smax = -1;
+            foreach (int curr in nums)
+            {
+                if (curr > max)
+                {
+                    smax = max;
+                    max = curr;
+
+                }
+                else if (curr > smax)
+                {
+                    smax = curr;
+                }
+            }
+            return (max - 1) * (smax - 1);
         }
     }
 }
