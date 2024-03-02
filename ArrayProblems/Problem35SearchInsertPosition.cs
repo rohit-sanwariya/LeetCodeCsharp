7788e3b8e6d4cfe2dc8299973c5352be29f18ffa
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-03-02 19:23:28 +0530

    BFS

---
+++ ArrayProblems/Problem35SearchInsertPosition.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems;
+
+public class Problem35SearchInsertPosition
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int l = 0, r = nums.Length - 1, pos = -1;
+        int min = int.MaxValue;
+        int i = -1;
+        int el = int.MaxValue;
+        while (l <= r)
+        {
+            int m = (l) + ((r - l) / 2);
+            if(Math.Abs(target - nums[m]) < min)
+            {
+                min = Math.Abs(target - nums[m]);
+                 i = m;
+                el = nums[m];
+            }
+            if (nums[m] == target)
+            {
+                pos = m;
+                break;
+            }
+            else if (target > nums[m]) {
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+        return pos == -1 ? (target > el  ? i + 1 : i) : pos;
+    }
+}
---
+++ GraphProblems/DepthFirstTraversal.cs
@@ -0,0 +1,49 @@
+﻿namespace GraphProblems;
+
+public class DepthFirstTraversal
+{
+    Dictionary<char, char[]> dict = new Dictionary<char, char[]>();
+    Stack<char> stack = [];
+    public void DepthFirstTraversalIterative()
+    {
+        
+        stack.Push('a');
+
+        while (stack.Count > 0)
+        {
+            char curr = stack.Pop();
+            Console.WriteLine(curr);
+            char[] neibhours = dict[curr];
+            foreach (char n in neibhours)
+            {
+                stack.Push(n);
+            }
+        }
+        // a,b,d
+        // a,b,c
+    }
+    public DepthFirstTraversal()
+    {
+        GenerateGraph();
+    }
+
+    public void DepthFirstTraversalRecursive(char source='a')
+    {
+        Console.WriteLine(source);
+        char[] neibhours = dict[source];
+        foreach(char n in neibhours)
+        {
+            DepthFirstTraversalRecursive(n);
+        }
+    }
+
+    private void GenerateGraph()
+    {
+        dict.Add('a', ['c', 'b']);
+        dict.Add('b', ['d']);
+        dict.Add('c', ['e']);
+        dict.Add('d', ['f']);
+        dict.Add('e', []);
+        dict.Add('f', []);
+    }
+}
