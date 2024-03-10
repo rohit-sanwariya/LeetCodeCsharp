bedb2fddd7fcf42f17741f2be545b1b2dcddc88f
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2024-03-10 08:16:33 +0530

    day 36.1

---
+++ ArrayProblems/Problem2540FindMinimumCommonInTwoArrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems;
+
+public class Problem2540FindMinimumCommonInTwoArrays
+{
+    public int GetCommon(int[] nums1, int[] nums2)
+    {
+        int l = 0,r=0;
+        while(l<nums1.Length && r < nums2.Length)
+        {
+            if (nums1[l] < nums2[r])
+            {
+                l++;
+            }
+            else if (nums1[l] < nums2[r])
+            {
+                r++;
+            }
+            else
+            {
+                return nums1[l];
+            }
+        }
+        return 0;
+
+    }
+}
---
+++ ArrayProblems/Problem349IntersectionOfTwoArrays.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems;
+
+public class Problem349IntersectionOfTwoArrays
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new();
+        HashSet<int> set2 = new();
+        foreach (int el in nums1)
+        {
+            set1.Add(el);
+        }
+        foreach (int el in nums2)
+        {
+            set2.Add(el);
+        }
+        set1.IntersectWith(set2);
+        return set1.ToArray();
+    }
+}
